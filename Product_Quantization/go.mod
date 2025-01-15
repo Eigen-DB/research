68de4aa2826d12d5ded3abd511709d48977312e6
Ryan Awad <ryanawad@yahoo.com>
2025-01-15 18:36:58 -0500

    Wrote some rough Go code for product quantization

---
+++ Product_Quantization/go.mod
@@ -0,0 +1,3 @@
+module product_quantization
+
+go 1.23.2
---
+++ Product_Quantization/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand/v2"
+)
+
+func generateRandomVector(dim int) []float32 {
+	vec := make([]float32, dim)
+	for i := 0; i < dim; i++ {
+		vec[i] = rand.Float32()*200 - 100
+	}
+	return vec
+}
+
+func computeL2Distance(u []float32, v []float32) float64 {
+	dist := 0.0
+	for i := 0; i < len(u); i++ {
+		dist += math.Pow(float64(u[i]-v[i]), 2)
+	}
+	return math.Sqrt(dist)
+}
+
+func generateSubVectors(m int, vector []float32) [][]float32 {
+	subVectors := make([][]float32, m)
+	D_ := len(vector) / m
+	for i := 0; i < m; i++ {
+		subVectors[i] = vector[i*D_ : (i+1)*D_]
+	}
+	return subVectors
+}
+
+func generateCentroids(k int, m int, D_ int) [][][]float32 {
+	if k%m != 0 {
+		panic("k must be divisible by m")
+	}
+
+	centroids := make([][][]float32, m)
+	k_ := k / m
+	for i := range m {
+		centroids[i] = make([][]float32, k_)
+		for j := range k_ {
+			centroids[i][j] = make([]float32, D_)
+			centroids[i][j] = generateRandomVector(D_)
+		}
+	}
+	return centroids
+}
+
+func compressVector(subVectors [][]float32, centroids [][][]float32) []int {
+	compressed := make([]int, len(subVectors))
+	for i := range len(subVectors) {
+		minDist := math.MaxFloat64
+		for j := range len(centroids[i]) {
+			dist := computeL2Distance(subVectors[i], centroids[i][j])
+			if dist < minDist {
+				minDist = dist
+				compressed[i] = j
+			}
+		}
+	}
+	return compressed
+}
+
+func decompressVector(compressed []int, centroids [][][]float32) []float32 {
+	decompressed := make([]float32, 0)
+	for i := range len(compressed) {
+		decompressed = append(decompressed, centroids[i][compressed[i]]...)
+	}
+	return decompressed
+}
+
+func main() {
+	num_vectors := 5
+	dim := 12
+	vectors := make([][]float32, num_vectors)
+	for i := range num_vectors {
+		vectors[i] = generateRandomVector(dim)
+		fmt.Println(vectors[i])
+	}
+
+	m := 4
+	subVectors := make([][][]float32, num_vectors)
+	for i := range num_vectors {
+		subVectors[i] = generateSubVectors(m, vectors[i])
+		//fmt.Println(subVectors[i])
+	}
+
+	k := int(math.Pow(2, 10))
+	centroids := generateCentroids(k, m, dim/m)
+	//fmt.Println(centroids)
+
+	compressedVectors := make([][]int, num_vectors)
+	for i := range num_vectors {
+		compressedVectors[i] = compressVector(subVectors[i], centroids)
+		fmt.Println(compressedVectors[i])
+	}
+
+	for i := range num_vectors {
+		decompressed := decompressVector(compressedVectors[i], centroids)
+		fmt.Println(decompressed)
+	}
+}
